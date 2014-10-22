bd98d22def75fe06badfa728ecc9221c56f2137e
dhanke2 <dhanke2@smail.uni-koeln.de>
2014-10-22 12:16:30 +0200

    Aufgabe 1

--- Aufgabe 1/src/Application.java
+++ Aufgabe 1/src/Application.java
@@ -2,13 +2,13 @@
 public class Application {
 
 	public static void main(String[] args) {
-		Fehler f = new Fehler;
-		f.variable = 10;
-		f.variable2 = 10
+		Fehler f = new Fehler(); //6
+		f.variable1 = 10; //7
+		f.variable2 = 10; //8
 		f.variable3 = 20;
 		f.methode1();
 		f.methode2();
-		f.methode 3();
+		f.methode3(); //10
 		System.out.println("Alle Fehler sind verschwunden!");
 	}
 
--- Aufgabe 1/src/Fehler.java
+++ Aufgabe 1/src/Fehler.java
@@ -1,21 +1,22 @@
 
 class Fehler {
 
-	Integer variable1
+	Integer variable1; //1
 	
-	Integer variable 2;
+	Integer variable2; //2
 	
-	String variable3;
+	Integer variable3; //9 und 11
 	
-	Boolean variable3;
+	Boolean variable4; //3
 	
-	void methode1 {
+	void methode1() { //4
 		System.out.println("Methode 1 aufgerufen");
 	}
 	
 	void methode2() {
 		System.out.println("Methode 2 aufgerufen");
-	
+	} //5
+		
 	void methode3() {
 		System.out.println("Methode 3 aufgerufen");
 	}
